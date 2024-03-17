7ee66e660ccc0454811d28e949db100ee78dad68
khaouitiabdelhakim <abdelhakimkhaouiti62@gmail.com>
2024-03-17 15:56:46 +0000

    example changes

--- example.js
+++ example.js
@@ -1,10 +1,5 @@
-const modules = require('./index');
+const quran = require('./index');
 
-hasfsHolyQuran = modules.HolyQuranHafsVersion;
+hasfsHolyQuran = quran.QuranEnglish;
 
-// Check if hasfsHolyQuran[0] exists before accessing its properties
-if (hasfsHolyQuran[0] && hasfsHolyQuran[0]["verses"]) {
-    console.log(hasfsHolyQuran[0]["verses"][2]);
-} else {
-    console.log("Error: Unable to access verses.");
-}
+console.log(hasfsHolyQuran[3][12]);
